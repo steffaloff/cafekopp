f666cde73f7a2fd255ec07be1e9d067c2da4cde1
Stephan Pettersson <stephan.pettersson@capgemini.com>
2023-02-08 14:14:54 +0100

    edit next.config.js syntax

--- next.config.js
+++ next.config.js
@@ -1,19 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-const azureimages = {
-    images: {
-        domains: ['cafekoppstorage.blob.core.windows.net'],
+  images: {
+        /**domains: ['cafekoppstorage.blob.core.windows.net'],*/
         remotePatterns: [
         {
             protocol: 'https',
             hostname: 'cafekoppstorage.blob.core.windows.net',
             port: '',
             pathname: '/*/*',
-        }]
-    }
+        },],
+    },
 }
 
-module.exports = nextConfig, azureimages
+
+module.exports = nextConfig
